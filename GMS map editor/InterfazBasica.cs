f7218f8b6762b4d3c7acbe1f7791d5cdbddca456
eltantillo <eltantillo@gmail.com>
2016-03-09 13:02:59 -0700

    UPDATE

--- GMS map editor/InterfazBasica.cs
+++ GMS map editor/InterfazBasica.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
+using System.IO;
 
 namespace WindowsFormsApplication1{
     public partial class InterfazBasica : Form{
@@ -38,7 +40,48 @@ namespace WindowsFormsApplication1{
 
         private void noseToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Load Existing Project
 
+            StringBuilder output = new StringBuilder();
+
+            String xmlString = File.ReadAllText(@"D:\Projects\GMLCoder\testProject.gmx\testProject.project.gmx").ToString();
+
+            MessageBox.Show(xmlString);
+
+            // Create an XmlReader
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
+            {
+                reader.ReadToFollowing("background");
+                MessageBox.Show(reader.ReadElementContentAsString());
+
+
+
+                //reader.ReadToFollowing("title");
+                //output.AppendLine("Content of the title element: " + reader.ReadElementContentAsString());
+
+                /*while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            output.AppendLine(reader.Name);
+                            break;
+                        case XmlNodeType.Text:
+                            output.AppendLine(reader.Value);
+                            break;
+                        case XmlNodeType.XmlDeclaration:
+                        case XmlNodeType.ProcessingInstruction:
+                            //writer.WriteProcessingInstruction(reader.Name, reader.Value);
+                            break;
+                        case XmlNodeType.Comment:
+                            output.AppendLine(reader.Value);
+                            break;
+                        case XmlNodeType.EndElement:
+                            //writer.WriteFullEndElement();
+                            break;
+                    }
+                }*/
+            }
         }
 
         private void deleteDelToolStripMenuItem_Click(object sender, EventArgs e)
--- GMS map editor/Program.cs
+++ GMS map editor/Program.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using System.Xml;
-using System.IO;
-using System.Text;
 
 namespace WindowsFormsApplication1
 {
@@ -16,55 +13,6 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
-            //Load Existing Project
-
-            StringBuilder output = new StringBuilder();
-
-            String xmlString = File.ReadAllText(@"D:\Projects\GMLCoder\testProject.gmx\testProject.project.gmx").ToString();
-
-            MessageBox.Show(xmlString);
-
-            // Create an XmlReader
-            using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
-            {
-                reader.ReadToFollowing("backgrounds");
-                while (reader.Read())
-                {
-                    if (reader.ReadElementContentAsString() != ""){
-                        MessageBox.Show(reader.ReadElementContentAsString());
-                    }
-                }
-
-
-                //reader.ReadToFollowing("title");
-                //output.AppendLine("Content of the title element: " + reader.ReadElementContentAsString());
-
-                /*while (reader.Read())
-                {
-                    switch (reader.NodeType)
-                    {
-                        case XmlNodeType.Element:
-                            output.AppendLine(reader.Name);
-                            break;
-                        case XmlNodeType.Text:
-                            output.AppendLine(reader.Value);
-                            break;
-                        case XmlNodeType.XmlDeclaration:
-                        case XmlNodeType.ProcessingInstruction:
-                            //writer.WriteProcessingInstruction(reader.Name, reader.Value);
-                            break;
-                        case XmlNodeType.Comment:
-                            output.AppendLine(reader.Value);
-                            break;
-                        case XmlNodeType.EndElement:
-                            //writer.WriteFullEndElement();
-                            break;
-                    }
-                }*/
-            }
-
-            MessageBox.Show(output.ToString());
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new InterfazBasica());

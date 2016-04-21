ddb9eb58c6300a6bb08df2f8e2fb698a83253d63
MarcoM X <MarcoM X>
2016-04-21 08:58:12 -0600

    UPDATE

--- GMS map editor/MainWindow.Designer.cs
+++ GMS map editor/MainWindow.Designer.cs
@@ -223,10 +223,10 @@
             this.div_left.TabIndex = 2;
             this.div_left.MouseClick += new System.Windows.Forms.MouseEventHandler(this.div_left_MouseClick);
             // 
-            // pb
+            // tileBox
             // 
             this.tileBox.Location = new System.Drawing.Point(3, 182);
-            this.tileBox.Name = "pb";
+            this.tileBox.Name = "tileBox";
             this.tileBox.Size = new System.Drawing.Size(0, 0);
             this.tileBox.TabIndex = 2;
             this.tileBox.TabStop = false;
@@ -254,19 +254,20 @@
             this.div_center.Controls.Add(this.mapBox);
             this.div_center.Location = new System.Drawing.Point(0, 0);
             this.div_center.Name = "div_center";
-            this.div_center.Size = new System.Drawing.Size(594, 339);
+            this.div_center.Size = new System.Drawing.Size(597, 339);
             this.div_center.TabIndex = 3;
             // 
-            // pb2
+            // mapBox
             // 
             this.mapBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.mapBox.Location = new System.Drawing.Point(3, 184);
-            this.mapBox.Name = "pb2";
+            this.mapBox.Name = "mapBox";
             this.mapBox.Size = new System.Drawing.Size(0, 0);
             this.mapBox.TabIndex = 3;
             this.mapBox.TabStop = false;
-            this.mapBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb2_MouseClick);
-            this.mapBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pb2_MouseMove);
+            this.mapBox.Click += new System.EventHandler(this.mapBox_Click);
+            this.mapBox.Paint += new System.Windows.Forms.PaintEventHandler(this.mapBox_Paint);
+            this.mapBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mapBox_MouseMove);
             // 
             // resourcesTabs
             // 
@@ -278,7 +279,7 @@
             this.resourcesTabs.Location = new System.Drawing.Point(0, 0);
             this.resourcesTabs.Name = "resourcesTabs";
             this.resourcesTabs.SelectedIndex = 0;
-            this.resourcesTabs.Size = new System.Drawing.Size(158, 341);
+            this.resourcesTabs.Size = new System.Drawing.Size(161, 341);
             this.resourcesTabs.TabIndex = 0;
             // 
             // roomsTab
@@ -288,7 +289,7 @@
             this.roomsTab.Location = new System.Drawing.Point(4, 22);
             this.roomsTab.Margin = new System.Windows.Forms.Padding(0);
             this.roomsTab.Name = "roomsTab";
-            this.roomsTab.Size = new System.Drawing.Size(150, 315);
+            this.roomsTab.Size = new System.Drawing.Size(153, 315);
             this.roomsTab.TabIndex = 0;
             this.roomsTab.Text = "Rooms";
             // 
@@ -308,7 +309,7 @@
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(150, 315);
+            this.tabPage4.Size = new System.Drawing.Size(153, 315);
             this.tabPage4.TabIndex = 1;
             this.tabPage4.Text = "tabPage4";
             // 
--- GMS map editor/MainWindow.cs
+++ GMS map editor/MainWindow.cs
@@ -12,10 +12,8 @@ using GMSMapEditor.ProjectAssets;
 using GMSMapEditor.ProjectAssets.Grafico;
 using System.Drawing.Drawing2D;
 
-namespace GMSMapEditor
-{
-    public partial class MainWindow : Form
-    {
+namespace GMSMapEditor{
+    public partial class MainWindow : Form{
         int tilePositionXO, tilePositionYO, tilePositionXF, tilePositionYF;
         int mapPositionX, mapPositionY;
         int tileWH;
@@ -25,8 +23,7 @@ namespace GMSMapEditor
         bool clicked;
         Bitmap canvas;
 
-        public MainWindow()
-        {
+        public MainWindow(){
             InitializeComponent();
             splitContainer1.SplitterWidth = 3;
             splitContainer2.SplitterWidth = 3;
@@ -68,37 +65,27 @@ namespace GMSMapEditor
             Project.SaveProject();
         }
 
-        private void toggleProjectFiles_Click(object sender, EventArgs e)
-        {
+        private void toggleProjectFiles_Click(object sender, EventArgs e){
             splitContainer1.Panel2Collapsed = !splitContainer1.Panel2Collapsed;
         }
 
         private void closeProjectToolStripMenuItem_Click(object sender, EventArgs e){
-            tileWH = 32; // tileWH = width and height of tile in pixel //
-            int mapSizeSimple = 100; // mapSizeSimple = size in pixel / tileWH //
+            tileWH = 32;
+            int mapSizeSimple = 32;
             
-            //Simple file chooser//
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             openFileDialog1.FilterIndex = 1;
             openFileDialog1.Multiselect = false;
             openFileDialog1.ShowDialog();
 
-            //ini backgroundTile//
-            // constructor = FILE(IMG), tile width in pixel, tile height in pixel;
             bt = new BackgroundTile(openFileDialog1.FileName, tileWH, tileWH);
             
-            //draw background tile reference of PictureBox;
             ((BackgroundTile)bt).drawBackgroundTile(tileBox);
 
-            //ini SimpleRoom
-            // constructor = width in simple mode, height in simple mode, tile width in pixel,tile height in pixel
             sr = new SimpleRoom(mapSizeSimple, mapSizeSimple, tileWH, tileWH);
 
-            // ini mapBox //
             mapBox.Location = new Point(0, 0);
-            // set map size height //
             mapBox.Height = mapSizeSimple * tileWH;
-            // set map size width //
             mapBox.Width = mapSizeSimple * tileWH;
             
             // Bitmap to draw all graphics //
@@ -106,7 +93,6 @@ namespace GMSMapEditor
         }
 
         private void div_left_MouseClick(object sender, MouseEventArgs e){
-            // click event //
             if (tileBox.Image != null){
                 if (clicked){
                     clicked = false;
@@ -124,111 +110,44 @@ namespace GMSMapEditor
 
         private void pb_MouseMove(object sender, MouseEventArgs e){
             if (e.Button == System.Windows.Forms.MouseButtons.Left && tileBox.Image != null){
-                // some stuff idk //
                 Control control = (Control)sender;
                 if (control.Capture){
                     control.Capture = false;
                 }
                 if (control.ClientRectangle.Contains(e.Location)){
-                    // back to my code xD //
-                    // if mouse position change //
                     if (tilePositionXF != (int)Math.Floor((Decimal)e.Location.X / 32)){
-                        // redraw//
                         tileBox.Image = bt.getClonedImage();
                     }
                     if (tilePositionYF != (int)Math.Floor((Decimal)e.Location.Y / 32)){
                         tileBox.Image = bt.getClonedImage();
                     }
                     
-                    // transform the real coords of mouse to grid position //
                     tilePositionXF = bt.simplificacion(e.Location.X);
                     tilePositionYF = bt.simplificacion(e.Location.Y);
 
-                    // change selection //
                     bt.setSelection(new Point(tilePositionXO, tilePositionYO), new Point(tilePositionXF, tilePositionYF));
 
-                    // redraw the rectangle //
                     tileBox.Image = bt.drawRectangle();
                     tileBox.Refresh();
-                }
-            }
-        }
-        private void pb2_MouseMove(object sender, MouseEventArgs e){
-            // if move //
-            if (mapPositionX != ((int)Math.Floor((Decimal)e.Location.X / tileWH) * tileWH) + tileWH){
-                // change mapbox image //
-                mapBox.Image = (Image) sr.tuneada.Clone();
-                mapBox.Refresh();
-            }
-            if (mapPositionY != ((int)Math.Floor((Decimal)e.Location.Y / tileWH) * tileWH) + tileWH){
-                mapBox.Image = (Image)sr.tuneada.Clone();
-                mapBox.Refresh();
-            }
-
-            mapPositionX = ((int)Math.Floor((Decimal)e.Location.X / tileWH) * tileWH) + tileWH;
-            mapPositionY = ((int)Math.Floor((Decimal)e.Location.Y / tileWH) * tileWH) + tileWH;
-
-            // draw selected tileImage //
-            Graphics mapArea = mapBox.CreateGraphics();
-            Image i = bt.getSelectedImage();
-            if (i != null){
-                mapArea.DrawImage(
-                    i,
-                    new Point(mapPositionX - (bt.difx * tileWH), mapPositionY - (bt.dify * tileWH))
-                );
-            }
-
-            if (e.Button == System.Windows.Forms.MouseButtons.Left){
-                Control control = (Control)sender;
-                if (control.Capture){
-                    control.Capture = false;
-                }
-                if (control.ClientRectangle.Contains(e.Location)){
-                    mapBox.Image = setImage();
-                    sr.tuneada = mapBox.Image as Bitmap;
-                    for (int x = bt.difx - 1; x >= 0; x--)
-                    {
-                        for (int y = bt.dify - 1; y >= 0; y--)
-                        {
-                            sr.setImage(
-                                ((int)Math.Floor((Decimal)e.Location.X / tileWH)) - x,
-                                ((int)Math.Floor((Decimal)e.Location.Y / tileWH)) - y,
-                                new Point(tilePositionXO + bt.difx - 1 - x, tilePositionYO + bt.dify - 1 - y)
-                            );
-                        }
-                    }
-                    //sr.echoMatrix();
+                    sr.updateSelected(bt.getSelectedImage());
                 }
             }
         }
 
-        private void pb2_MouseClick(object sender, MouseEventArgs e){
-            mapBox.Image = setImage();
-            sr.tuneada = mapBox.Image as Bitmap;
-            for (int x = bt.difx-1; x >=0 ; x--){
-                for (int y = bt.dify-1; y >= 0; y--){
-                    sr.setImage(
-                        ((int)Math.Floor((Decimal)e.Location.X / tileWH))-x,
-                        ((int)Math.Floor((Decimal)e.Location.Y / tileWH))-y,
-                        new Point(tilePositionXO + bt.difx -1 - x, tilePositionYO + bt.dify -1 - y)
-                    );
-                }
-            }
-            //sr.echoMatrix();
+
+
+        private void mapBox_Paint(object sender, PaintEventArgs e){
+            sr.update(mapBox, mapPositionX, mapPositionY);
         }
 
-        public Bitmap setImage(){
-            Graphics mapArea = Graphics.FromImage(canvas);
-            Image i = bt.getSelectedImage();
-            if (i != null)
-            {
-                mapArea.DrawImage(
-                    i,
-                    new Point(mapPositionX - (bt.difx * tileWH), mapPositionY - (bt.dify * tileWH))
-                );
-            }
-            sr.setImage(mapBox.Image);
-            return canvas;
+        private void mapBox_Click(object sender, EventArgs e){
+            sr.click(mapBox, mapPositionX, mapPositionY);
+        }
+
+        private void mapBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            mapPositionX = sr.toGrid(e.Location.X, SimpleRoom.TO_X);
+            mapPositionY = sr.toGrid(e.Location.Y, SimpleRoom.TO_Y);
         }
     }
 }
